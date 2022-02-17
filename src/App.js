05d253212651ed4525dd4b0029283aeb750604c1
gaurav8628 <gauravverma0803@gmail.com>
2022-02-17 20:55:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useMoralis,
+  useMoralisWeb3Api,
+  useMoralisWeb3ApiCall,
+} from "react-moralis";
+import {useEffect, useState} from "react";
+import {Button,Center,Box,Input,Divider} from "@chakra-ui/react";
+import {Moralis} from 'moralis';
 
 function App() {
+  const {web3,enableWeb3,isWeb3Enabled} = useMoralis();
+  const Web3Api = useMoralisWeb3Api();
+  const [domainName, setDomainName] = useState("");
+  const [ensAddress, setEnsAddress] = useState("");
+  
+  const handleDomainChange = (event) => setDomainName(event.target.value);
+
+  const {
+    fetch: fetchAddressForDomain,
+    data,
+    error,
+    isLoading,
+  } = useMoralisWeb3ApiCall(
+    Web3Api.resolve.resolveDomain,
+    {
+      currency: "eth",
+      domain: domainName,
+    },
+    {autoFetch: false}
+  );
+
+  const ensCall = async() => {
+    // web3.eth.ens.getAddress(domainName).then( (address) => {
+    //   setEnsAddress(address);
+    // }).
+    // catch(err => {console.log(err)})
+    console.log(domainName);
+    const options = { currency: 'eth', domain: domainName };
+const resolve = await Moralis.Web3API.resolve.resolveDomain(options );
+console.log(resolve);
+
+  }
+
+  useEffect(() => {
+    if(!isWeb3Enabled) {
+      enableWeb3();
+    }
+  },[isWeb3Enabled,enableWeb3]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Box display={"block"} p={35} className="App">
+     <Center>
+       <Input
+        value={domainName}
+        onChange = {handleDomainChange}
+        placeholder = "enter domain name"
+        size="sm"
+        style={{
+          height:"auto",
+          width:"50%"
+        }}
+       />
+       <Button
+        onClick={fetchAddressForDomain}
+        style={{color:"blue", fontWeight: "700"}}
+       >
+         Resolve the Unstopable Domain{" "}
+       </Button>
+       <Button onClick={ensCall} style={{color:"blue", fontWeight:"700"}}>
+         Resolve theENS Domain{" "}
+       </Button>
+     </Center>
+     <br />
+     <Center style={{fontWeight:"700"}}>
+       Unstopable Domains:{" "}
+       {data ? data.address: "Enter valid Unstoppable domain"}
+     </Center>
+     <Center style={{fontWeight: "700"}}>
+       Ens Domain: {ensAddress ? ensAddress: "Enter valid Ens Domain"}
+     </Center>
+
+   </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {MoralisProvider} from 'react-moralis';
+import {ChakraProvider, extendTheme}  from '@chakra-ui/react';
 
+const theme = extendTheme({
+  config: {
+    initialColorMode: "light",
+  },
+});
+
+// const moralisAppId = "rpwIHrPvItjQU3UDqNWab2ILKPmUBFdPEYpmaWA3";
+// const moralisServerUrl = "https://k5dzq9it9wlw.usemoralis.com:2053/server";
+const moralisAppId="oekGhbuq0kfiNBgktbq1shqT7jmW4Djci0DbZ6Jn";
+const moralisServerUrl="https://wmieztcay6uu.usemoralis.com:2053/server";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={moralisAppId} serverUrl={moralisServerUrl}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </MoralisProvider>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
